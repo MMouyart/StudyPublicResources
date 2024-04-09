9b0921a42caaac20f78624d7c7f1913c2d84ea4a
MMouyart <matthieumouyart34@gmail.com>
2024-04-09 18:54:14 +0200

    adding learning resources

--- script.sh
+++ script.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
    path="$HOME/.var/app/com.brave.Browser/config/BraveSoftware/Brave-Browser/Default/Bookmarks"
    i=0
+   type=`cat ${path} | jq .roots.bookmark_bar.children[${i}].type`
+   type="${type:1:-1}"
    name=`cat ${path} | jq .roots.bookmark_bar.children[${i}]`
    while [ "${name}" != null ]
    do
 	   file=`cat ${path} | jq .roots.bookmark_bar.children[${i}].name`
 	   file="${file:1:-1}"
-	   cat "${path}" | jq .roots.bookmark_bar.children["${i}"] > "${file}"
-	   i=$((i+1))
-	   name=`cat ${path} | jq .roots.bookmark_bar.children[${i}]`
+	   if [ "${file}" = "/e/os" ] || [ "${file}" = "iot cyber" ] || [ "${file}" = "stage" ] || [ "${file}" = "recherche" ] || [ "${file}" = "autre" ] || [ "${file}" = "emploi" ] || [ "${type}" = "url" ]
+	   then
+		   i=$((i+1))
+		   name=`cat ${path} | jq .roots.bookmark_bar.children[${i}]`
+                   type=`cat ${path} | jq .roots.bookmark_bar.children[${i}].type`
+                   type="${type:1:-1}"
+	   else
+		   cat "${path}" | jq .roots.bookmark_bar.children["${i}"] > "${file}.json"
+	           i=$((i+1))
+		   name=`cat ${path} | jq .roots.bookmark_bar.children[${i}]`
+                   type=`cat ${path} | jq .roots.bookmark_bar.children[${i}].type`
+                   type="${type:1:-1}"
+	   fi
    done
 
