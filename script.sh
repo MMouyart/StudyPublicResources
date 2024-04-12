d8cdc98d9592a3d0dc203525226710327871dd2b
MMouyart <matthieumouyart34@gmail.com>
2024-04-12 10:13:58 +0200

    Update bookmarks extraction script

--- script.sh
+++ script.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
    path="$HOME/.var/app/com.brave.Browser/config/BraveSoftware/Brave-Browser/Default/Bookmarks"
    i=0
-   type=`cat ${path} | jq .roots.bookmark_bar.children[${i}].type`
-   type="${type:1:-1}"
-   name=`cat ${path} | jq .roots.bookmark_bar.children[${i}]`
+   j=0
+   name=`cat ${path} | jq .roots.bookmark_bar.children[${j}].name`
+   name="${name:1:-1}"
+   while [ "${name}" != "Learning" ]
+   do
+	   j=$((j+1))
+           name=`cat ${path} | jq .roots.bookmark_bar.children[${j}].name`
+           name="${name:1:-1}"
+   done
+   name=`cat ${path} | jq .roots.bookmark_bar.children[${j}].children[${i}]`
    while [ "${name}" != null ]
    do
-	   file=`cat ${path} | jq .roots.bookmark_bar.children[${i}].name`
+	   file=`cat ${path} | jq .roots.bookmark_bar.children[${j}].children[${i}].name`
 	   file="${file:1:-1}"
-	   if [ "${file}" = "/e/os" ] || [ "${file}" = "iot cyber" ] || [ "${file}" = "stage" ] || [ "${file}" = "recherche" ] || [ "${file}" = "autre" ] || [ "${file}" = "emploi" ] || [ "${type}" = "url" ]
-	   then
-		   i=$((i+1))
-		   name=`cat ${path} | jq .roots.bookmark_bar.children[${i}]`
-                   type=`cat ${path} | jq .roots.bookmark_bar.children[${i}].type`
-                   type="${type:1:-1}"
-	   else
-		   cat "${path}" | jq .roots.bookmark_bar.children["${i}"] > "${file}.json"
-	           i=$((i+1))
-		   name=`cat ${path} | jq .roots.bookmark_bar.children[${i}]`
-                   type=`cat ${path} | jq .roots.bookmark_bar.children[${i}].type`
-                   type="${type:1:-1}"
-	   fi
+	   cat "${path}" | jq .roots.bookmark_bar.children["${j}"].children["${i}"] > "${file}.json"
+	   i=$((i+1))
+           name=`cat ${path} | jq .roots.bookmark_bar.children[${j}].children[${i}]`
    done
-
